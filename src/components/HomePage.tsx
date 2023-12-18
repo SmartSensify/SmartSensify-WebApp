c52d034f3d65d18c3b28d2e4ba742a1ff8e37c12
Jakub Krok <50359069+Brtgkrk@users.noreply.github.com>
2023-12-18 23:40:13 +0100

    New smartsensify api v0.7

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -7,6 +7,8 @@ const HomePage = () => {
       <h2>Home</h2>
       <p>Welcome to the home page!</p>
       <Link to="/login">Go to Login</Link>
+      <h3>All sensors:</h3>
+      <Link to="/sensors">link</Link>
     </div>
   );
 };
