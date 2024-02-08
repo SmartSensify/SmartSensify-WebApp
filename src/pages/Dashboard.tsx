6d053d2c32ad6a2a7d8aa063443aed1072253b92
Jakub Krok <50359069+Brtgkrk@users.noreply.github.com>
2024-02-08 23:15:07 +0100

    Dashboard layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,6 +6,10 @@ import { useNavigate } from 'react-router-dom';
 import { removeAuthToken } from '../utils/auth';
 import { Link } from 'react-router-dom';
 
+// Styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/dashboardPage.css';
+
 const Dashboard: React.FC = () => {
     const navigate = useNavigate();
     const [userData, setUserData] = useState<any>(null);
@@ -24,8 +28,8 @@ const Dashboard: React.FC = () => {
     };
 
     return (
-        <div>
-            <h2>Dashboard</h2>
+        <div className="container-fluid vh-100" id="page-dashboard">
+            {/* <h2>Dashboard</h2>
             {userData && (
                 <div>
                     <p>Username: {userData.username}</p>
@@ -34,9 +38,76 @@ const Dashboard: React.FC = () => {
                 </div>
             )}
             <button onClick={handleLogout}>Logout</button>
-            <Link to="/dashboard/groups">Groups</Link> 
+            <Link to="/dashboard/groups">Groups</Link> */}
+            {/* TODO: Make this link relative */}
+
+            {/* Left Menu Pane */}
+            <div className="row h-100 overflow-hidden">
+                <nav className="col-md-3 col-lg-2 d-md-block bg-success sidebar text-white p-3">
+                    <div className="text-center">
+                        <img id="image-logo" className="img-fluid mb-3" src="/images/smartsensify_logo_white.png" alt="Logo"/>
+                    </div>
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#">
+                                Overview
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Alerts
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/dashboard/groups">Groups
+                                Sensors
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Settings
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="mt-auto">
+                        <button className="btn btn-light btn-block mb-2">
+                            <i className="bi bi-google"></i> Contact Us
+                        </button>
+                        <button className="btn btn-light btn-block" onClick={handleLogout}>
+                            <i className="bi bi-google"></i> Logout
+                        </button>
+                    </div>
+                </nav>
+
+                {/* Right Pane */}
+                <div className="col-md-9 col-lg-10 px-0">
+                    {/* Header */}
+                    <header className="d-flex justify-content-between align-items-center bg-white p-2">
+                        <div>
+                            Welcome back @
+                            {userData && (
+                                <>
+                                    {userData.username}
+                                </>
+                            )}
+                        </div>
+                        {userData && (
+                            <>
+                                {userData.email}
+                            </>
+                        )}
+                    </header>
+
+                    {/* Main Content */}
+                    <main className="p-3 scrollable-container">
+                    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+                    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+                    <br />hello<br />hello<br />hello<br />hello<br />hello<br /><br /><br /><br /><br /><br /><br />
+                    </main>
+                </div>
+            </div>
         </div>
-    ); // TODO: Make this link relative
+    );
 };
 
 export default Dashboard;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 // Utils
@@ -13,10 +13,30 @@ const Login: React.FC = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [loginError, setLoginError] = useState<string | null>(null);
+    const [isUsernameInvalid, setIsUsernameInvalid] = useState(false);
+    const [isPasswordInvalid, setIsPasswordInvalid] = useState(false);
 
-    const handleLogin = async () => {
+    const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault(); 
 
-        // Validate forms first
+        if (!username) {
+            setIsUsernameInvalid(true);
+        } else {
+            setIsUsernameInvalid(false);
+        }
+
+        if (!password) {
+            setIsPasswordInvalid(true);
+        } else {
+            setIsPasswordInvalid(false);
+        }
+
+        if (!username || !password) return;
+
+        const loadingElements = document.querySelectorAll('.submit-loading');
+        loadingElements.forEach((element) => {
+            element.classList.remove('visually-hidden');
+        });
 
         try {
             await loginUser({
@@ -28,27 +48,16 @@ const Login: React.FC = () => {
             navigate('/dashboard');
 
         } catch (error) {
+            loadingElements.forEach((element) => {
+                element.classList.add('visually-hidden');
+            });
             console.error('Login failed:', error);
             setLoginError('Login failed. Please check your credentials.');
         }
     };
 
     return (
-        <div>
-            {/* <h2>Login</h2>
-            {loginError && <div style={{ color: 'red' }}>{loginError}</div>}
-            <div>
-                <label>Username:</label>
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-            </div>
-            <div>
-                <label>Password:</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <button onClick={handleLogin}>Login</button> */}
-
-            {/* NEW CODE BELOW */}
-
+        <div id="page-login">
             <div className="container-fluid d-flex align-items-center justify-content-center vh-100">
                 <div className="rounded p-3" style={{ maxWidth: '350px', height: '650px', backgroundColor: 'rgba(0,0,0,0.8)' }}>
                     <img
@@ -57,50 +66,54 @@ const Login: React.FC = () => {
                         className="img-fluid mb-5"
                     />
                     <div className="mb-5">
-                        <input
-                            type="text"
-                            className={`form-control mb-4 ${!username && 'is-invalid'}`}
-                            placeholder="Username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            required
-                        />
-                        {(!username && (
-                            <div className="invalid-feedback">
-                                Please enter a username.
-                            </div>
-                        ))}
+                        <form className="row g-3 needs-validation" id='login-form' onSubmit={handleLogin} noValidate>
+                            <input
+                                type="text"
+                                className={`form-control ${isUsernameInvalid && 'is-invalid'}`}
+                                placeholder="Username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                            />
+                            {(isUsernameInvalid && (
+                                <div className="invalid-feedback">
+                                    Please enter a username.
+                                </div>
+                            ))}
 
-                        <input
-                            type="password"
-                            className={`form-control mb-4 ${!password && 'is-invalid'}`}
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                        {(!password && (
-                            <div className="invalid-feedback">
-                                Please enter a password.
+                            <input
+                                type="password"
+                                className={`form-control mt-4 ${isPasswordInvalid && 'is-invalid'}`}
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                            {(isPasswordInvalid && (
+                                <div className="invalid-feedback">
+                                    Please enter a password.
+                                </div>
+                            ))}
+                            <div className="d-flex justify-content-between align-items-center mt-4 mb-2">
+                                <div className="form-check">
+                                    <input type="checkbox" className="form-check-input" id="rememberMe" />
+                                    <label className="form-check-label" htmlFor="rememberMe">
+                                        Remember me
+                                    </label>
+                                </div>
+                                <a href="#" className="ml-3 text-light">
+                                    Forgot password?
+                                </a>
                             </div>
-                        ))}
-                        <div className="d-flex justify-content-between align-items-center mb-2">
-                            <div className="form-check">
-                                <input type="checkbox" className="form-check-input" id="rememberMe" />
-                                <label className="form-check-label" htmlFor="rememberMe">
-                                    Remember me
-                                </label>
-                            </div>
-                            <a href="#" className="ml-3 text-light">
-                                Forgot password?
-                            </a>
-                        </div>
+                        </form>
                     </div>
+                    {loginError && <div className="text-danger">{loginError}</div>}
 
-                    {loginError && <div style={{ color: 'red' }}>{loginError}</div>}
-
-                    <button className="btn btn-primary mb-3 w-100" onClick={handleLogin}>
+                    <button className="btn btn-primary mb-3 w-100" type="submit" form="login-form">
+                        <span className="submit-loading visually-hidden spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                         Login
+                        <span className="submit-loading visually-hidden">...</span>
+                        {/* {document.querySelector('.submit-loading.visually-hidden') ? null : 'Login'} */}
                     </button>
 
                     <div className='mb-5'>
